49f1ea5fe37884974cfc0d11bf193ee77b8cb1b0
Ruben Rodriguez <tul52111@temple.edu>
2023-03-21 19:18:54 -0400

    created API

---
+++ WebApplication3/Controllers/ProductController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web.Http;
+using WebApplication3.Models;
+
+namespace WebApplication3.Controllers
+{
+    public class ProductController : ApiController
+    {
+       
+
+        Product[] products = new Product[]
+        {
+            new Product { Id = 1, Name = "Tomato Soup", Category = "Groceries", Price = 1 },
+            new Product { Id = 2, Name = "Yo-yo", Category = "Toys", Price = 3.75M },
+            new Product { Id = 3, Name = "Hammer", Category = "Hardware", Price = 16.99M }
+        };
+
+        public ProductController()
+        {
+        }
+        public ProductController(Product[] products)
+        {
+            this.products = products;
+        }
+
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return products;
+        }
+
+        public async Task<IEnumerable<Product>> GetAllProductsAsync()
+        {
+            return await Task.FromResult(GetAllProducts());
+        }
+
+        public IHttpActionResult GetProduct(int id)
+        {
+            var product = products.FirstOrDefault((p) => p.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+        }
+
+        public async Task<IHttpActionResult> GetProductAsync(int id)
+        {
+            return await Task.FromResult(GetProduct(id));
+        }
+    }
+}
